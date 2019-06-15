49a49bc0863449ca2b390edb12ba4748592374c8
wangxingkang <wangxingkang@sensoro.com>
2019-06-15 23:46:55 +0800

    feat: 去除自定义图例

--- src/pie/pie-chart.tsx
+++ src/pie/pie-chart.tsx
@@ -34,7 +34,6 @@ export interface IProps {
   selected?: boolean;
   // 指定图表的高度，单位为 'px'
   height?: number;
-  hasLegend?: boolean;
   // 图表内边距
   padding?: TPadding;
   data?: IDataItem[];
@@ -50,17 +49,8 @@ export interface IProps {
   percent?: number;
   // 图表的宽度自适应开关
   forceFit?: boolean;
-  valueFormat?: (value: string) => string | React.ReactNode;
   // 获取 chart 实例的回调
   onGetG2Instance?: (chart: G2.Chart) => void;
-}
-
-export interface ILegendDataItem {
-  checked: boolean;
-  x: string;
-  color: string;
-  percent: number;
-  y: string;
 }
 
 const scale = {
@@ -72,9 +62,6 @@ const scale = {
     min: 0,
   },
 };
-
-let chartInstance: G2.Chart = null;
-let requestRef = null;
 
 const PieChart: React.FC<IProps> = (props) => {
   const {
@@ -90,84 +77,14 @@ const PieChart: React.FC<IProps> = (props) => {
     title,
     subTitle,
     total,
-    hasLegend,
-    valueFormat,
     innerRadius,
     lineWidth,
     onGetG2Instance
   } = props;
   const rootRef = React.useRef(null);
-  const [legendBlock, setLegendBlock] = React.useState<boolean>(false);
-  const [legendData, setLegendData] = React.useState<ILegendDataItem[]>([]);
-
-  React.useEffect(() => {
-    window.addEventListener('resize', () => {
-      requestRef = requestAnimationFrame(() => resize());
-    }, { passive: true })
-  }, []);
-
-  React.useEffect(() => {
-    console.log('******');
-    getLegendData();
-  }, [props.data]);
 
   const handleGetG2Instance = (chart: G2.Chart) => {
-    chartInstance = chart;
     onGetG2Instance && onGetG2Instance(chart);
-  };
-
-  // 用于自定义图例
-  const getLegendData = () => {
-    if (!chartInstance) return;
-    const geom = chartInstance.getAllGeoms()[0];
-    if (!geom) return;
-    // @ts-ignore
-    const items = geom.get('dataArray') || [];
-
-    const data = items.map((item: { color: any; _origin: any }[]) => {
-      const origin = item[0]._origin;
-      origin.color = item[0].color;
-      origin.checked = true;
-      return origin;
-    });
-
-    setLegendData(data);
-  };
-
-  const handleLegendClick = (item: any, i: string | number) => {
-    const newItem = item;
-    newItem.checked = !newItem.checked;
-    const newLegendData = [...legendData];
-    newLegendData[i] = newItem;
-
-    const filteredLegendData = newLegendData.filter(l => l.checked).map(l => l.x);
-
-    if (chartInstance) {
-      chartInstance.filter('x', val => filteredLegendData.indexOf(val + '') > -1);
-    }
-
-    setLegendData(newLegendData);
-  };
-
-  const resize = () => {
-    const root: HTMLDivElement = rootRef.current;
-
-    if (!hasLegend || !root) {
-      window.removeEventListener('resize', resize);
-      return;
-    }
-
-    if (
-      root &&
-      root.parentNode &&
-      (root.parentNode as HTMLElement).clientWidth <= 380
-    ) {
-      if (!legendBlock) {
-        setLegendBlock(true);
-      }
-    } else {
-      setLegendBlock(false);
-    }
   };
 
   const tooltipFormat: [
@@ -217,14 +134,11 @@ const PieChart: React.FC<IProps> = (props) => {
     as: 'percent',
   });
 
-
   return (
     <div
       ref={rootRef}
       className={classNames(className, {
-        [`${prefixCls}`]: true,
-        [`show-legend`]: !!hasLegend,
-        ['legend-block']: legendBlock
+        [`${prefixCls}`]: true
       })}
       style={style}
     >
@@ -273,24 +187,6 @@ const PieChart: React.FC<IProps> = (props) => {
           )}
         </div>
       </FitText>
-
-      {hasLegend && (
-        <ul className={`${prefixCls}__legend`}>
-          {legendData.map((item, i) => (
-            <li key={item.x} onClick={() => handleLegendClick(item, i)}>
-              <span
-                className={`${prefixCls}__dot`}
-                style={{
-                  backgroundColor: !item.checked ? '#aaa' : item.color,
-                }}
-              />
-              <span className={`${prefixCls}__legend-title`}>
-                {item.x}
-              </span>
-            </li>
-          ))}
-        </ul>
-      )}
     </div>
   )
 };
@@ -298,7 +194,6 @@ const PieChart: React.FC<IProps> = (props) => {
 PieChart.defaultProps = {
   animate: true,
   forceFit: true,
-  hasLegend: false,
   height: 400,
   innerRadius: 0.75,
   lineWidth: 1,
