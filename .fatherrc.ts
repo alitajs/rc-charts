b1a99948fe5829d9a77aa3d06741feb609a3ef34
wangxingkang <wangxingkang@sensoro.com>
2019-06-22 16:47:54 +0800

    feat: 添加文档目录

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,4 +1,5 @@
 import { IBundleOptions } from 'father';
+import path from 'path'
 
 const options: IBundleOptions = {
   esm: {
@@ -8,7 +9,17 @@ const options: IBundleOptions = {
   cjs: 'rollup',
   extraBabelPlugins: [
     ['import', { libraryName: 'antd', style: true }]
-  ]
+  ],
+  // @ts-ignore
+  doc: {
+    modifyBundlerConfig: (config) => {
+      config['resolve'].alias = Object.assign({}, config['resolve'].alias, {
+        '@charts': path.resolve(__dirname, 'src'),
+      });
+      return config;
+    },
+    menu: []
+  }
 };
 
 export default options;
