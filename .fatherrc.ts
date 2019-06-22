eef4b294a9257f43983dd4153ae18087cf3c4ed9
wangxingkang <wangxingkang@sensoro.com>
2019-06-22 18:36:02 +0800

    feat: 优化配置，添加logo

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,6 +1,9 @@
 import { IBundleOptions } from 'father';
-import path from 'path'
+import path from 'path';
 
+const env = process.env.NODE_ENV;
+
+// @ts-ignore
 const options: IBundleOptions = {
   esm: {
     type: 'rollup',
@@ -13,14 +16,30 @@ const options: IBundleOptions = {
   // @ts-ignore
   doc: {
     base: '/rc-charts',
+    propsParser: false,
     modifyBundlerConfig: (config) => {
       config['resolve'].alias = Object.assign({}, config['resolve'].alias, {
         '@charts': path.resolve(__dirname, 'src'),
       });
       return config;
     },
+    // @ts-ignore
+    htmlContext: {
+      favicon: env === 'development'
+        ? '/public/favicon.png'
+        : '/rc-charts/public/favicon.png'
+    },
     public: 'docs/public',
     indexHtml: 'docs/index.html',
+    themeConfig: {
+      logo: {
+        src: env === 'development'
+          ? '/public/logo.svg'
+          : '/rc-charts/public/logo.svg',
+        width: '60px',
+      },
+    },
+    title: 'Rc Charts'
   }
 };
 
