d7751df345276d37fc25151c90a3ca13d24707a9
wangxingkang <wangxingkang@sensoro.com>
2019-07-02 12:53:50 +0800

    feat: 代码优化

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,5 @@ export { default as Line } from './line';
 export { default as Bar } from './bar';
 export { default as Area } from './area';
 export { Calendar } from './heat-map';
+export { default as Radar } from './radar';
 
