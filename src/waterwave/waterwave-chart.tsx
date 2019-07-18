ca8adb0251dc6da54578185b8f876b396e29627f
chenshuhang <437132124@qq.com>
2019-07-18 18:15:47 +0800

    feat：水波图完成

--- src/waterwave/waterwave-chart.tsx
+++ src/waterwave/waterwave-chart.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import classNames from 'classnames';
-import { isArray } from 'awe-utils';
-import { TPadding } from '../global';
-import Title, { TPosition } from '../components/title';
-
-import styles from './waterwave-chart.less';
-import { string } from 'prop-types';
-
-const prefixCls = 'rc-line-chart';
 
 export interface IWaterWaveChartProps {
+  className?: string;
   height?: number;
-  title?: string;
-  titlePosition?: TPosition;
-  percent?: number;
+  subTitle: string;
+  percent: number;
   color?: string;
   titleSize?: string;
   titleColor?: string;
@@ -24,46 +15,71 @@ export interface IWaterWaveChartProps {
 }
 
 
-const WaterWaveChart: React.FC<IWaterWaveChartProps> = (props) =>  {
+class WaterWaveChart extends React.Component<IWaterWaveChartProps>  {
 
-  const {
-    height,
-    title,
-    titlePosition,
-    percent,
-    color,
-    titleSize,
-    titleColor,
-    titleHeight,
-    percentColor,
-    percentSize,
-    percentHeight,
-  } = props;
+  state = {
+    radio: 1,
+  };
 
-  var node: string;
+  timer: number = 0;
 
-  const [radio, setRadio] = React.useState(1);
-  var root =  document.getElementById('root');
-  const offsetWidth = document.getElementById('root').offsetWidth;
-  // console.log(root);
-  // console.log(root.parentNode);
-  // console.log(offsetWidth);
-  function resize () {
-      setRadio(offsetWidth < height ? offsetWidth / height : 1);
+  root: HTMLDivElement | undefined | null = null;
+
+  node: HTMLCanvasElement | undefined | null = null;
+
+  componentDidMount() {
+    this.renderChart();
+    this.resize();
+    window.addEventListener(
+      'resize',
+      () => {
+        requestAnimationFrame(() => this.resize());
+      },
+      { passive: true },
+    );
   }
 
-  function renderChart() {
+  componentDidUpdate(props: IWaterWaveChartProps) {
+    const { percent } = this.props;
+    if (props.percent !== percent) {
+      // 不加这个会造成绘制缓慢
+      this.renderChart('update');
+    }
+  }
+
+  componentWillUnmount() {
+    cancelAnimationFrame(this.timer);
+    if (this.node) {
+      this.node.innerHTML = '';
+    }
+    window.removeEventListener('resize', this.resize);
+  }
+
+  resize = () => {
+    if (this.root) {
+      const { height = 1 } = this.props;
+      const { offsetWidth } = this.root.parentNode as HTMLElement;
+      this.setState({
+        radio: offsetWidth < height ? offsetWidth / height : 1,
+      });
+    }
+  };
+
+  renderChart(type?: string) {
+    const { percent, color = '#1890FF' } = this.props;
     const data = percent / 100;
     const self = this;
-    // cancelAnimationFrame(this.timer);
-    const node = document.getElementById('canvas_id');
-    console.log(node);
-    if (node || (data !== 0 && !data)) {
+    cancelAnimationFrame(this.timer);
+
+    if (!this.node || (data !== 0 && !data)) {
       return;
     }
 
-    const canvas = node;
+    const canvas = this.node;
     const ctx = canvas.getContext('2d');
+    if (!ctx) {
+      return;
+    }
     const canvasWidth = canvas.width;
     const canvasHeight = canvas.height;
     const radius = canvasWidth / 2;
@@ -75,27 +91,30 @@ const WaterWaveChart: React.FC<IWaterWaveChartProps> = (props) =>  {
 
     const axisLength = canvasWidth - lineWidth;
     const unit = axisLength / 8;
-    const range = 0.2;  // 振幅
+    const range = 0.2; // 振幅
     let currRange = range;
     const xOffset = lineWidth;
-    let sp = 0;   // 周期偏移量
+    let sp = 0; // 周期偏移量
     let currData = 0;
     const waveupsp = 0.005; // 水波上涨速度
 
-    let arcStack = [];
+    let arcStack: number[][] = [];
     const bR = radius - lineWidth;
     const circleOffset = -(Math.PI / 2);
     let circleLock = true;
 
-    for(let i = circleOffset; i < circleOffset + 2 * Math.PI; i += 1 / (8 * Math.PI)) {
+    for (let i = circleOffset; i < circleOffset + 2 * Math.PI; i += 1 / (8 * Math.PI)) {
       arcStack.push([radius + bR * Math.cos(i), radius + bR * Math.sin(i)]);
     }
 
-    const cStartPoint = arcStack.shift();
+    const cStartPoint = arcStack.shift() as number[];
     ctx.strokeStyle = color;
     ctx.moveTo(cStartPoint[0], cStartPoint[1]);
 
     function drawSin() {
+      if (!ctx) {
+        return;
+      }
       ctx.beginPath();
       ctx.save();
 
@@ -110,7 +129,7 @@ const WaterWaveChart: React.FC<IWaterWaveChartProps> = (props) =>  {
         sinStack.push([dx, dy]);
       }
 
-      const startPoint = sinStack.shift();
+      const startPoint = sinStack.shift() as number[];
 
       ctx.lineTo(xOffset + axisLength, canvasHeight);
       ctx.lineTo(xOffset, canvasHeight);
@@ -125,26 +144,29 @@ const WaterWaveChart: React.FC<IWaterWaveChartProps> = (props) =>  {
     }
 
     function render() {
+      if (!ctx) {
+        return;
+      }
       ctx.clearRect(0, 0, canvasWidth, canvasHeight);
       if (circleLock && type !== 'update') {
         if (arcStack.length) {
-          const temp = arcStack.shift();
+          const temp = arcStack.shift() as number[];
           ctx.lineTo(temp[0], temp[1]);
           ctx.stroke();
         } else {
           circleLock = false;
           ctx.lineTo(cStartPoint[0], cStartPoint[1]);
           ctx.stroke();
-          arcStack = null;
+          arcStack = [];
 
           ctx.globalCompositeOperation = 'destination-over';
           ctx.beginPath();
           ctx.lineWidth = lineWidth;
-          ctx.arc(radius, radius, bR, 0, 2 * Math.PI, 1);
+          ctx.arc(radius, radius, bR, 0, 2 * Math.PI, true);
 
           ctx.beginPath();
           ctx.save();
-          ctx.arc(radius, radius, radius - 3 * lineWidth, 0, 2 * Math.PI, 1);
+          ctx.arc(radius, radius, radius - 3 * lineWidth, 0, 2 * Math.PI, true);
 
           ctx.restore();
           ctx.clip();
@@ -186,61 +208,69 @@ const WaterWaveChart: React.FC<IWaterWaveChartProps> = (props) =>  {
     render();
   }
 
- 
+  render() {
 
-  React.useEffect(() => {
-    renderChart();
-    resize();
-    window.addEventListener(
-      'resize',
-      () => {
-        requestAnimationFrame(() => this.resize());
-      },
-      { passive: true }
-    );
-    if (props.percent !== percent) {
-      // 不加这个会造成绘制缓慢
-      this.renderChart('update');
-    };
+    const {
+      height,
+      subTitle,
+      percent,
+      titleSize,
+      titleColor,
+      titleHeight,
+      percentColor,
+      percentSize,
+      percentHeight,
+    } = this.props;
 
-    return () => {
-      // cancelAnimationFrame(this.timer);
-      // if (this.node) {
-      //   this.node.innerHTML = '';
-      // }
-      window.removeEventListener('resize', this.resize);
-    }
-  }, [props]);
+    const { radio } = this.state;
 
-  var _this = this;
-
-  return (
-    <div
-      className={styles.waterWave}
-      // ref={n => (this.root = n)}
-      style={{ transform: `scale(${radio})` }}
-    >
-      <div style={{ width: height, height, overflow: 'hidden' }}>
-        <canvas
-          id={'canvas_id'}
-          className={styles.waterWaveCanvasWrapper}
-          // ref={n => (this.node = n)}
-          // ref={n => (document.getElementById('canvas_id') = n)}
-          width={height * 2}
-          height={height * 2}
-        />
+    return (
+      <div
+        ref={n => (this.root = n)}
+        style={{ 
+          display: `flex`,
+          transform: `scale(${radio})`,
+          position: `relative`,
+          transformOrigin: `left`,
+          width: `100%`,
+          justifyContent: 'center'
+        }}
+      >
+        <div style={{ width: height, height, overflow: 'hidden' }}>
+          <canvas
+            style={{transform: `scale(0.5)`, transformOrigin: `0 0`}}
+            ref={n => (this.node = n)}
+            width={ height*2  }
+            height={ height *2 }
+          />
+        </div>
+        <div 
+          style={{ 
+            width: `100%`, 
+            position: `absolute`,
+            top: `32px`,
+            left: 0,
+            textAlign: `center`,
+          }}
+        >
+          {subTitle && <span style={{fontSize: titleSize, color: titleColor, lineHeight: titleHeight}}>{subTitle}</span>}
+          <h4 style={{fontSize: percentSize, color: percentColor, lineHeight: percentHeight}}>{percent}%</h4>
+        </div>
       </div>
-      <div className={styles.text} style={{ width: height }}>
-          {title && <span>{title}</span>}
-          <h4>{percent}%</h4>
-      </div>
-    </div>
-  )
-}
+    )
+  }
+};
 
 WaterWaveChart.defaultProps = {
-  height: 100,
-}
+  height: 400,
+  color: '#1890FF',
+  titleSize: '24px',
+  titleColor: 'rgb(132, 133, 135)',
+  titleHeight: '22px',
+  percentSize: '24px',
+  percentHeight: '32px',
+  percentColor: 'black',
+};
 
 export default WaterWaveChart;
 
