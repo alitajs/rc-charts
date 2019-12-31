ac4b15de81e8e3d7f0293bcadd6ad40cbe5730fa
王林涛 <lintao.wang@yitu-inc.com>
2019-12-31 14:33:33 +0800

    fix(pie): fix #22

--- src/pie/pie-chart.tsx
+++ src/pie/pie-chart.tsx
@@ -96,7 +96,7 @@ const defaultScale = {
 };
 
 const PieChart: React.FC<PieProps> = (props) => {
-  let chartInstance: G2.Chart = null;
+  const chartRef = React.useRef<G2.Chart>(null);
   let requestRef = null;
   const prefixCls = 'rc-pie-chart';
   const rootRef = React.useRef(null);
@@ -156,8 +156,8 @@ const PieChart: React.FC<PieProps> = (props) => {
 
   // 用于自定义图例
   const getLegendData = () => {
-    if (!chartInstance) return;
-    const geom = chartInstance.getAllGeoms()[0];
+    if (!chartRef.current) return;
+    const geom = chartRef.current.getAllGeoms()[0];
     if (!geom) return;
     // @ts-ignore
     const items = geom.get('dataArray') || [];
@@ -180,8 +180,8 @@ const PieChart: React.FC<PieProps> = (props) => {
 
     const filteredLegendData = newLegendData.filter(l => l.checked).map(l => l.x);
 
-    if (chartInstance) {
-      chartInstance.filter('x', val => filteredLegendData.indexOf(val + '') > -1);
+    if (chartRef.current) {
+      chartRef.current.filter('x', val => filteredLegendData.indexOf(val + '') > -1);
     }
 
     setLegendData(newLegendData);
@@ -209,7 +209,7 @@ const PieChart: React.FC<PieProps> = (props) => {
   };
 
   const handleGetG2Instance = (chart: G2.Chart) => {
-    chartInstance = chart;
+    chartRef.current = chart;
     setInnerWidth(chart.get('height') * innerRadius);
     onGetG2Instance && onGetG2Instance(chart);
   };
